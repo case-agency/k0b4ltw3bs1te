0641a84dce1a97dae4b5f960ec78267059aa6341
Shelby Foster <shelby.foster@kobaltmusic.com>
2015-12-11 16:56:58 -0800

    WEB-3928 - [Kobalt Website] - Update Contact -> Submissions Page

--- page-contact-submissions.php
+++ page-contact-submissions.php
@@ -21,8 +21,18 @@ include("includes/data.php");
 
 <h3 class="black">KOBALT SUBMISSIONS POLICY</h3>
 
-<p class="black">We do not accept any unsolicited material. However, if you have an existing catalogue or released songs for which you are seeking administration/collection services, please email us at <a class="email" href="mailto:sales@kobaltmusic.com?subject=Submissions">sales@kobaltmusic.com</a>.</p>
+<p class="black">We appreciate your interest in Kobalt. Please consult the list below and email the appropriate Kobalt division.</p>
+<p class="black">Please note that we do not accept any unsolicited material, and request that you do not send over audio files. </p>
+<div class="submission-emails">
+  <p class="black">For Kobalt Music Publishing submissions, please email <a href="mailto:KMPsubmissions@kobaltmusic.com">KMPsubmissions@kobaltmusic.com</a>.</p>
+  <p class="black">For Kobalt Label Services submissions, please email <a href="mailto:KLSsubmissions@kobaltmusic.com">KLSsubmissions@kobaltmusic.com</a>.</p>
+  <p class="black">For Kobalt Neighbouring Rights submissions, please email <a href="mailto:KNRsubmissions@kobaltmusic.com">KNRsubmissions@kobaltmusic.com</a>.</p>
+</div>
 
+
+<p class="black">For information and contact details of AWAL (Artists Without A Label), please visit <a href="https://awal.com/contact">https://awal.com/contact</a>.</p>
+
+<p class="black submissions-help">Not sure what you need? You can email us at <a href="mailto:sales@kobaltmusic.com">sales@kobaltmusic.com</a> for help.</p>
 </div>
 </section>
 <?php include("includes/footer.php"); ?>
