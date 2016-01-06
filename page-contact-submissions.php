41dd9c90d72361f361130ecf144c952e53839f43
Shelby Foster <shelby.foster@kobaltmusic.com>
2016-01-06 13:43:38 -0800

    WEB-3952 - [Kobalt Website] - Update Contact Submissions Page

--- page-contact-submissions.php
+++ page-contact-submissions.php
@@ -22,17 +22,17 @@ include("includes/data.php");
 <h3 class="black">KOBALT SUBMISSIONS POLICY</h3>
 
 <p class="black">We appreciate your interest in Kobalt. Please consult the list below and email the appropriate Kobalt division.</p>
-<p class="black">Please note that we do not accept any unsolicited material, and request that you do not send over audio files. </p>
+<p class="black">Please note that we do not accept any unsolicited submissions of unreleased material. We will only review existing catalogues, and request that you only send web links to accompany your submission. Audio files (MP3s, etc.) will be ignored and will not be downloaded.</p>
 <div class="submission-emails">
-  <p class="black">For Kobalt Music Publishing submissions, please email <a href="mailto:KMPsubmissions@kobaltmusic.com">KMPsubmissions@kobaltmusic.com</a>.</p>
-  <p class="black">For Kobalt Label Services submissions, please email <a href="mailto:KLSsubmissions@kobaltmusic.com">KLSsubmissions@kobaltmusic.com</a>.</p>
-  <p class="black">For Kobalt Neighbouring Rights submissions, please email <a href="mailto:KNRsubmissions@kobaltmusic.com">KNRsubmissions@kobaltmusic.com</a>.</p>
+  <p class="black">For Kobalt Music Publishing submissions, please email <a href="mailto:KMP.submissions@kobaltmusic.com">KMP.submissions@kobaltmusic.com</a>.</p>
+  <p class="black">For Kobalt Label Services submissions, please email <a href="mailto:KLS.submissions@kobaltmusic.com">KLS.submissions@kobaltmusic.com</a>.</p>
+  <p class="black">For Kobalt Neighbouring Rights submissions, please email <a href="mailto:KNR.submissions@kobaltmusic.com">KNR.submissions@kobaltmusic.com</a>.</p>
 </div>
 
 
 <p class="black">For information and contact details of AWAL (Artists Without A Label), please visit <a href="https://awal.com/contact">https://awal.com/contact</a>.</p>
 
-<p class="black submissions-help">Not sure what you need? You can email us at <a href="mailto:sales@kobaltmusic.com">sales@kobaltmusic.com</a> for help.</p>
+<p class="black submissions-help">--Not sure what you need? You can email us at <a href="mailto:info@kobaltmusic.com">info@kobaltmusic.com</a> for help.</p>
 </div>
 </section>
 <?php include("includes/footer.php"); ?>
